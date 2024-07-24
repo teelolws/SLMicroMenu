4d3e4b9278b0a8d4065af0b01e00b9cdb761ccfc
Teelo <49792789+teelolws@users.noreply.github.com>
2024-07-24 18:11:22 +1200

    Update for 11.0

--- Source/SLMicroMenu.lua
+++ Source/SLMicroMenu.lua
@@ -47,8 +47,8 @@ end
 
 local buttons = {
     {button = CharacterMicroButton, name = "Character"},
-    {button = SpellbookMicroButton, name = "Spellbook"},
-    {button = TalentMicroButton, name = "Talents"},
+    {button = ProfessionMicroButton, name = "Spellbook"},
+    {button = PlayerSpellsMicroButton, name = "Talents"},
     {button = AchievementMicroButton, name = "Achievement"},
     {button = QuestLogMicroButton, name = "Quest"},
     {button = GuildMicroButton, name = "Socials"},
